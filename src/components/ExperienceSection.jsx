c8c286ed1dfd40f92b85339503d5458c40d12c4c
kamalpandeydev-dev <kamalpandey.dev@gmail.com>
2026-02-28 20:02:44 +0530

    Freelance work Updated

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -7,17 +7,34 @@ const ExperienceSection = () => (
     aria-labelledby="experience-heading"
   >
     <h2 className="projects-heading">Professional Experience</h2>
-<div className="job">
-      <h3>
-        Freelance UI/UX Designer & Frontend Developer For Sookti AI | Crazyeggz | Pratual Solar Power| 
-      </h3>
-      <p className="job-meta">Delhi/NCR, India | May 2025 – Present</p>
+    <div className="job">
+      <h3>Freelance UI/UX Designer & Frontend Developer</h3>
+      <p className="job-meta">Delhi/NCR, India | May 2025 - Present</p>
       <ul>
         <li>
-          Designed and developed accessible UI interfaces using React.js and SCSS, Figma Design System with accessibility.
+          Designed and developed accessible UI interfaces using React.js and
+          SCSS, Figma Design System with accessibility.
         </li>
         <li>
-          https://sookti.ai/ | https://crazyeggz.in/about-us/ | https://www.pratualsolar.com/about
+          <a href="https://sookti.ai/" target="_blank" alt="Sookti Ai">
+            Sookti AI
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://crazyeggz.in/about-us/"
+            target="_blank"
+            alt="Crazyeggz"
+          >
+            Crazyeggz
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://www.pratualsolar.com/about"
+            target="_blank"
+            alt="Pratual Solar Power"
+          >
+            Pratual Solar Power
+          </a>
         </li>
       </ul>
     </div>
